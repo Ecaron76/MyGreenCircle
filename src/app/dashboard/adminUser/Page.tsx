9ee1f14d2d7a214a5be99a5e6746be8e6b0a5f67
hanane <hananemouder55@gmail.com>
2024-06-02 17:06:07 +0200

    import data to test, and display confirmation delete modale

--- src/app/dashboard/adminUser/Page.tsx
+++ src/app/dashboard/adminUser/Page.tsx
@@ -1,74 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box } from "@mui/material";
 import { GridActionsCellItem, GridColDef } from "@mui/x-data-grid";
 import DeleteIcon from "@mui/icons-material/Delete";
 import DataGridComponent from "../components/dataGrid/Page";
-import AddButton from "../components/AddButton/Page";
-
-const columns: GridColDef<(typeof rows)[number]>[] = [
-  {
-    field: "firstName",
-    headerName: "Nom",
-    width: 200,
-    editable: true,
-  },
-  {
-    field: "lastName",
-    headerName: "Prenom",
-    width: 200,
-    editable: true,
-  },
-  {
-    field: "email",
-    headerName: "Email",
-    width: 300,
-    editable: true,
-  },
-  {
-    field: "role",
-    headerName: "Role",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 200,
-    valueGetter: (value, row) => `${row.firstName || ""} ${row.lastName || ""}`,
-  },
-  {
-    field: "actions",
-    type: "actions",
-    headerName: "Actions",
-    width: 100,
-    getActions: (row) => [
-      <GridActionsCellItem
-        icon={<DeleteIcon />}
-        label="Delete"
-        onClick={() => handleDelete(row.row.id)}
-      />,
-    ],
-  },
-];
-
-function handleDelete(id: number) {
-  console.log("Deleting row with id:", id);
-}
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", email: 14 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", email: 31 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", email: 31 },
-  { id: 4, lastName: "Stark", firstName: "Arya", email: 11 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", email: null },
-  { id: 6, lastName: "Melisandre", firstName: null, email: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", email: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", email: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", email: 65 },
-];
+import ModalDelete from "../components/modalDelete/Page";
+import rowsData from "./users.json";
+import { User } from "../types/types";
 
 function AdminUser() {
+  const [rows, setRows] = useState<any>(rowsData);
+  const [open, setOpen] = useState<boolean>(false);
+  const [deleteId, setDeleteId] = useState<number | null>(null);
+
+  const handleClickOpen = (id: number) => {
+    setDeleteId(id);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleDelete = (id: number) => {
+    setRows(rows.filter((row: any) => row.id !== id));
+    handleClose();
+  };
+
+  const columns: GridColDef<User>[] = [
+    {
+      field: "firstName",
+      headerName: "Nom",
+      width: 200,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Prenom",
+      width: 200,
+      editable: true,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 300,
+      editable: true,
+    },
+    {
+      field: "role",
+      headerName: "Role",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 200,
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      getActions: ({ row }) => [
+        <GridActionsCellItem
+          icon={<DeleteIcon />}
+          label="Delete"
+          onClick={() => handleClickOpen(row.id)}
+        />,
+      ],
+    },
+  ];
+
   return (
     <Box sx={{ height: 400, width: "100%" }}>
-      <AddButton disabled={true}></AddButton>
-
-      <DataGridComponent rows={rows} columns={columns}></DataGridComponent>
+      <DataGridComponent
+        rows={rows}
+        columns={columns}
+        identifier="utilisateur"
+      />
+      <ModalDelete
+        open={open}
+        onClose={handleClose}
+        onConfirm={() => handleDelete(deleteId!)}
+        title="Confirmez la suppression"
+        message="Êtes-vous sûr de vouloir supprimer cet utilisateur ?"
+      />
     </Box>
   );
 }
