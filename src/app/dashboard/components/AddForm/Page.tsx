ce419c0d1f602a036cb121391310bd7c1dc549ed
hanane <hananemouder55@gmail.com>
2024-06-02 17:08:54 +0200

    use react hook form to control field when add form

--- src/app/dashboard/components/AddForm/Page.tsx
+++ src/app/dashboard/components/AddForm/Page.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
 import {
   Button,
   Card,
@@ -8,130 +9,170 @@ import {
 } from "@mui/material";
 import CloudUploadIcon from "@mui/icons-material/Download";
 import { useDropzone } from "react-dropzone";
+import AlertComponent from "../Alert/Page";
 
 function AddFormComponent({ typeForm }: any) {
   const { getRootProps, getInputProps } = useDropzone();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [showAlert, setShowAlert] = useState(false);
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    setShowAlert(true);
+  };
 
   return (
-    <Card
-      sx={{
-        maxWidth: 345,
-        mx: "auto",
-        mt: -5,
-        backgroundColor: "inherit",
-        boxShadow: "none",
-        border: "none",
-      }}
-    >
-      <CardContent
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Card
         sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+          maxWidth: 345,
+          mx: "auto",
+          mt: -5,
+          backgroundColor: "inherit",
+          boxShadow: "none",
+          border: "none",
         }}
       >
-        <Typography variant="h6" fontSize={20} color="#18534F" gutterBottom>
-          Ajout d'un nouveau {typeForm.slice(0, -1)}
-        </Typography>
-        <TextField
-          fullWidth
-          margin="normal"
-          label="Titre"
-          variant="outlined"
-          placeholder="Saisir un titre"
+        <CardContent
           sx={{
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": {
-                borderColor: "#226D68",
-              },
-              "&:hover fieldset": {
-                borderColor: "#226D68",
-              },
-              "&.Mui-focused fieldset": {
-                borderColor: "#226D68",
-              },
-            },
-            "& .MuiInputLabel-root": {
-              color: "#666666",
-              "&.Mui-focused": {
-                color: "#666666",
-              },
-            },
-          }}
-        />
-        <TextField
-          fullWidth
-          margin="normal"
-          label="Contenu"
-          variant="outlined"
-          placeholder="Saisir un contenu"
-          multiline
-          rows={4}
-          sx={{
-            "& .MuiOutlinedInput-root": {
-              "& fieldset": {
-                borderColor: "#FEEAA1",
-              },
-              "&:hover fieldset": {
-                borderColor: "#FEEAA1",
-              },
-              "&.Mui-focused fieldset": {
-                borderColor: "#FEEAA1",
-              },
-            },
-            "& .MuiInputLabel-root": {
-              color: "#666666",
-              "&.Mui-focused": {
-                color: "#666666",
-              },
-            },
-          }}
-        />
-        <Typography variant="subtitle1" sx={{ mt: 2 }}>
-          Ajouter une image
-        </Typography>
-        <div
-          {...getRootProps({ className: "dropzone" })}
-          style={{
             display: "flex",
             flexDirection: "column",
             alignItems: "center",
-            padding: "20px",
-            borderWidth: 4,
-            borderRadius: 2,
-            borderColor: "#eeeeee",
-            borderStyle: "double",
-            color: "#bdbdbd",
-            outline: "none",
-            width: "50%",
-            cursor: "pointer",
           }}
         >
-          <input {...getInputProps()} />
-          <CloudUploadIcon style={{ fontSize: 48, color: "#bdbdbd" }} />
-        </div>
-        <Button
-          variant="contained"
-          sx={{
-            backgroundColor: "#226D68",
-            py: 2,
-            borderRadius: "4px",
-            boxShadow: "none",
-            "&:hover": {
+          {showAlert && (
+            <AlertComponent title={`${typeForm} créé avec succès`} />
+          )}
+          <Typography variant="h6" fontSize={20} color="#18534F" gutterBottom>
+            Ajout d'un nouveau {typeForm.slice(0, -1)}
+          </Typography>
+          <Controller
+            name="titre"
+            control={control}
+            rules={{ required: "Le titre est requis" }}
+            render={({ field, fieldState }) => (
+              <TextField
+                {...field}
+                fullWidth
+                label="Titre"
+                variant="outlined"
+                placeholder="Saisir un titre"
+                error={!!fieldState.error}
+                helperText={fieldState.error ? fieldState.error.message : null}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: "#226D68",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "#226D68",
+                    },
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#226D68",
+                    },
+                  },
+                  "& .MuiInputLabel-root": {
+                    color: "#666666",
+                    "&.Mui-focused": {
+                      color: "#666666",
+                    },
+                  },
+                  marginBottom: 3,
+                }}
+              />
+            )}
+          />
+          <Controller
+            name="contenu"
+            control={control}
+            rules={{ required: "Le contenu est requis" }}
+            render={({ field, fieldState }) => (
+              <TextField
+                {...field}
+                fullWidth
+                label="Contenu"
+                variant="outlined"
+                placeholder="Saisir un contenu"
+                multiline
+                rows={4}
+                error={!!fieldState.error}
+                helperText={fieldState.error ? fieldState.error.message : null}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    "& fieldset": {
+                      borderColor: "#FEEAA1",
+                    },
+                    "&:hover fieldset": {
+                      borderColor: "#FEEAA1",
+                    },
+                    "&.Mui-focused fieldset": {
+                      borderColor: "#FEEAA1",
+                    },
+                  },
+                  "& .MuiInputLabel-root": {
+                    color: "#666666",
+                    "&.Mui-focused": {
+                      color: "#666666",
+                    },
+                  },
+                  marginBottom: 3,
+                }}
+              />
+            )}
+          />
+          <Typography variant="subtitle1" sx={{ mt: 2 }}>
+            Ajouter une image
+          </Typography>
+          <div
+            {...getRootProps({ className: "dropzone" })}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              padding: "20px",
+              borderWidth: 4,
+              borderRadius: 2,
+              borderColor: "#eeeeee",
+              borderStyle: "double",
+              backgroundColor: "#fafafa",
+              color: "#bdbdbd",
+              outline: "none",
+              width: "50%",
+              cursor: "pointer",
+            }}
+          >
+            <input {...getInputProps()} />
+            <CloudUploadIcon style={{ fontSize: 48, color: "#bdbdbd" }} />
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
               backgroundColor: "#226D68",
+              py: 2,
+              borderRadius: "4px",
               boxShadow: "none",
-            },
-            "&:active": {
-              boxShadow: "none",
-            },
-            transition: "none",
-            mt: 2,
-          }}
-        >
-          Enregistrer
-        </Button>
-      </CardContent>
-    </Card>
+              "&:hover": {
+                backgroundColor: "#226D68",
+                boxShadow: "none",
+              },
+              "&:active": {
+                boxShadow: "none",
+              },
+              transition: "none",
+              mt: 2,
+            }}
+          >
+            Enregistrer
+          </Button>
+        </CardContent>
+      </Card>
+    </form>
   );
 }
 
