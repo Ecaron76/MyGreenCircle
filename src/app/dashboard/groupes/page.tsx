4b608faa0d1382184b697d48de791623d800a3f0
Florian Meunier <florian.meunier@ofracar.fr>
2024-06-04 17:31:26 +0200

    API: Création de groupe avec liaision Utilisateur en tant que Admin OK |||

--- src/app/dashboard/groupes/page.tsx
+++ src/app/dashboard/groupes/page.tsx
@@ -4,10 +4,27 @@ import './groupes.css'
 import GroupCard from "@/components/UI/GroupCard/GroupCard";
 import MainButton from "@/components/UI/MainButton/MainButton";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
+import CreateGrpModal from "@/components/GroupePage/CreateGrpModal/CreateGrpModal";
 
 
 const GroupesPage = () => {
     const {data: session} = useSession()
+    const [isCreateGrpModalVisible, setIsCreateGrpModalVisible] = useState(false);
+
+
+    const handleCreateGrpClick = () => {
+        setIsCreateGrpModalVisible(true);
+      };
+
+      const closeCreateGrpModal = () => {
+        setIsCreateGrpModalVisible(false);
+      };
+
+      const handleSignupSuccess = () => {
+        setIsCreateGrpModalVisible(false);
+      };
+
     console.log(session)
     if (session?.user) {
         return (
@@ -22,7 +39,7 @@ const GroupesPage = () => {
                 <section className="creategrp-section">
                     <h2>Initiateur d&apos;actions collectives ?</h2>
                     <p>Créez votre groupe et rassemblez des citoyens pour agir ensemble pour l&apos;écologie !</p>
-                    <MainButton name="Créer un groupe" type="button" />
+                    <MainButton name="Créer un groupe" type="button" onClick={handleCreateGrpClick}/>
                 </section>
                 
                 <section className="groups-section">
@@ -47,6 +64,8 @@ const GroupesPage = () => {
                         <GroupCard author="Ecaron" title="EcoNormandie"  nbMember="5,2K"/>
                     </div>
                 </section>
+                {isCreateGrpModalVisible && <CreateGrpModal onClose={closeCreateGrpModal} onSuccess={handleSignupSuccess}/>}
+
             </main>
         );
     }
--- src/components/GroupePage/CreateGrpModal/CreateGrpModal.tsx
+++ src/components/GroupePage/CreateGrpModal/CreateGrpModal.tsx
@@ -1,21 +1,79 @@
 
 import React, { useState } from 'react';
 import './CreateGrpModal.css';
+import MainButton from '@/components/UI/MainButton/MainButton';
 
 type CreateGrpModalProps = {
   onClose: () => void;
+  onSuccess: () => void;
 };
 
-const CreateGrpModal: React.FC<CreateGrpModalProps> = ({ onClose }) => {
+const CreateGrpModal: React.FC<CreateGrpModalProps> = ({ onClose, onSuccess }) => {
   
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [groupName, setgroupName] = useState('');
+  const [groupDescription, setgroupDescription] = useState('');
+  const [groupLocation, setgroupLocation] = useState('');
+  
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const response = await fetch('/api/groupe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        groupName: groupName,
+        groupDescription: groupDescription,
+        groupLocation: groupLocation,
+        
+      })
+    })
+
+    if (response.ok) {
+      console.log("OK")
+      onSuccess(); 
+    }
     
+  };
 
   return (
     <div className="modal">
-      <div className="modal-content">
+      <div className="modal-content-register">
         <span className="close" onClick={onClose}>&times;</span>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="groupName">Nom</label>
+            <input 
+              type="text" 
+              id="groupName" 
+              value={groupName} 
+              onChange={(e) => setgroupName(e.target.value)} 
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="groupDescription">Description</label>
+            <input 
+              type="groupDescription" 
+              id="groupDescription" 
+              value={groupDescription} 
+              onChange={(e) => setgroupDescription(e.target.value)} 
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="groupLocation">Localisation</label>
+            <input 
+              type="groupLocation" 
+              id="groupLocation" 
+              value={groupLocation} 
+              onChange={(e) => setgroupLocation(e.target.value)} 
+              required 
+            />
+          </div>      
+          <MainButton name='Créer' type='submit'/> 
+        </form>
       </div>
     </div>
   );
