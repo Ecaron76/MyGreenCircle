e625ccf87fca60d5acb4df9d20e68ed9d46729d2
Florian Meunier <florian.meunier@ofracar.fr>
2024-06-10 17:12:17 +0200

    API - Update Post OK

--- src/app/api/post/item/[postId]/route.ts
+++ src/app/api/post/item/[postId]/route.ts
@@ -64,3 +64,58 @@ export const DELETE = async (req: Request, { params }: { params: { postId: numbe
         );
     }
 };
+
+
+export async function PATCH(req: Request, { params }: { params: { postId: string } }) {
+    try {
+        const session = await getAuthSession();
+
+        if (!session || !session.user) {
+            return NextResponse.json(
+                { message: 'Not Authenticated' },
+                { status: 403 }
+            );
+        }
+
+        const { postId } = params;
+        const body = await req.json();
+        const { title, content, picture } = body;
+
+        const post = await prisma.post.findUnique({
+            where: { postId: Number(postId) },
+            include: { group: true }
+        });
+
+        if (!post) {
+            return NextResponse.json({ message: 'Post not found' }, { status: 404 });
+        }
+
+        if (post.userId !== session.user.id) {
+            return NextResponse.json({ message: 'User is not authorized to modify this post' }, { status: 403 });
+        }
+
+        let isVisible = post.isVisible;
+
+        if (post.groupId) {
+            isVisible = false;
+        }
+
+        const updatedPost = await prisma.post.update({
+            where: { postId: Number(postId) },
+            data: {
+                title,
+                content,
+                picture,
+                isVisible
+            }
+        });
+
+        return NextResponse.json({ updatedPost }, { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { message: 'Something went wrong' },
+            { status: 500 }
+        );
+    }
+}
