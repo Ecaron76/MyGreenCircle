44b0df1144bc9b902da6c45bb9cf28fb59e6265e
hanane <hananemouder55@gmail.com>
2024-06-10 21:58:48 +0200

    add the api to delete groupe

--- src/app/api/groupe/route.ts
+++ src/app/api/groupe/route.ts
@@ -1,59 +1,57 @@
-import { getAuthSession } from "@/lib/auth"
-import { NextResponse } from "next/server"
+import { getAuthSession } from "@/lib/auth";
+import { NextResponse } from "next/server";
 
 export const POST = async (req: Request) => {
-    try {
-      const session = await getAuthSession();
-  
-      if (!session || !session.user) {
-        return NextResponse.json(
-          { message: 'Not Authenticated' },
-          { status: 403 }
-        );
-      }
-  
-      const body = await req.json();
-      const { groupName, groupDescription, groupLocation } = body;
-  
-      const group = await prisma.group.create({
-        data: {
-          groupName,
-          groupDescription,
-          groupLocation,
-        },
-      });
-  
-      const join = await prisma.join.create({
-        data: {
-          userId: session.user.id,
-          groupId: group.groupId,
-          role: 'admin',
-          isAccepted: true,
-          joiningDate: new Date(),
-        },
-      });
-  
-      console.log(group);
-      console.log(join);
-      return NextResponse.json({  group, join }, { status: 200 });
-  
-    } catch (error) {
-      console.error(error);
+  try {
+    const session = await getAuthSession();
+
+    if (!session || !session.user) {
       return NextResponse.json(
-        { message: 'Something went wrong' },
-        { status: 500 }
+        { message: "Not Authenticated" },
+        { status: 403 }
       );
     }
-  };
 
-  
+    const body = await req.json();
+    const { groupName, groupDescription, groupLocation } = body;
+
+    const group = await prisma.group.create({
+      data: {
+        groupName,
+        groupDescription,
+        groupLocation,
+      },
+    });
+
+    const join = await prisma.join.create({
+      data: {
+        userId: session.user.id,
+        groupId: group.groupId,
+        role: "admin",
+        isAccepted: true,
+        joiningDate: new Date(),
+      },
+    });
+
+    console.log(group);
+    console.log(join);
+    return NextResponse.json({ group, join }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+};
+
 export const GET = async (req: Request) => {
   try {
     const session = await getAuthSession();
 
     if (!session || !session.user) {
       return NextResponse.json(
-        { message: 'Not Authenticated' },
+        { message: "Not Authenticated" },
         { status: 403 }
       );
     }
@@ -64,7 +62,38 @@ export const GET = async (req: Request) => {
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { message: 'Something went wrong' },
+      { message: "Something went wrong" },
+      { status: 500 }
+    );
+  }
+};
+
+export const DELETE = async (req: Request) => {
+  try {
+    const session = await getAuthSession();
+
+    if (!session?.user) {
+      return NextResponse.json(
+        { message: "Not Authenticated" },
+        { status: 403 }
+      );
+    }
+
+    const body = await req.json();
+    const { groupId } = body;
+
+    await prisma.group.delete({
+      where: { groupId: groupId },
+    });
+
+    return NextResponse.json(
+      { message: "Group deleted successfully" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Something went wrong" },
       { status: 500 }
     );
   }
