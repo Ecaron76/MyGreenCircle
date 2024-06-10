ccc237813108e6d48a094f66f6f0a5ce559300bc
Florian Meunier <florian.meunier@ofracar.fr>
2024-06-10 14:21:02 +0200

    API - GET Groups Posts + Admin Posts

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -30,7 +30,7 @@ export const POST = async (req: Request) => {
                     { status: 403 }
                 );
             }
-            
+
             const userIsGroupAdmin = await prisma.join.findFirst({
                 where: {
                     userId: session.user.id,
@@ -82,6 +82,57 @@ export const POST = async (req: Request) => {
                 );
             }
         }
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(
+            { message: 'Something went wrong' },
+            { status: 500 }
+        );
+    }
+};
+
+
+export const GET = async (req: Request) => {
+    try {
+        const session = await getAuthSession();
+
+        if (!session || !session.user) {
+            return NextResponse.json(
+                { message: 'Not Authenticated' },
+                { status: 403 }
+            );
+        }
+
+        const adminPosts = await prisma.post.findMany({
+            where: {
+                groupId: null
+            }
+        });
+
+        const userGroups = await prisma.join.findMany({
+            where: {
+                userId: session.user.id,
+                isAccepted: true
+            },
+            select: {
+                groupId: true
+            }
+        });
+
+        let groupPosts: any[] = [];        
+        for (const group of userGroups) {
+            const posts = await prisma.post.findMany({
+                where: {
+                    groupId: group.groupId,
+                    isVisible: true
+                }
+            });
+            groupPosts = [...groupPosts, ...posts];
+        }
+
+        const allPosts = [...adminPosts, ...groupPosts];
+
+        return NextResponse.json(allPosts, { status: 200 });
     } catch (error) {
         console.error(error);
         return NextResponse.json(
