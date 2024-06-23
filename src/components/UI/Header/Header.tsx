433d2aa092cfa0ee84ade53c81de808b4dc8a98f
Ecaron76 <ecaronpro76@gmail.com>
2024-06-23 17:11:01 +0200

    Déconnexion OK

--- src/components/UI/Header/Header.tsx
+++ src/components/UI/Header/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import './Header.css'
 import Image from 'next/image';
 import Link from 'next/link';
+import { signOut } from 'next-auth/react';
+
 
 type HeaderProps = {
     username: string,
@@ -23,6 +25,7 @@ const Header: React.FC<HeaderProps> = ({  username  }) => {
             <div className='header-profile'>
                 <Image alt='' src='/assets/images/logo.png' width={50} height={50} />
                 <p>{username}</p>
+                <button className='signout-btn' onClick={() => signOut()}> Se déconnecter </button>
             </div>
         </header>
     )
