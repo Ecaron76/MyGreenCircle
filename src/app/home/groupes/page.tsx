fcdd444c3d3d012ffbdb79e7046ebc8baf5d8dd2
Ecaron76 <ecaronpro76@gmail.com>
2024-06-27 20:41:28 +0200

    Rework PostCard EventCard GroupCard Header(Menu burger sur mobile)

--- src/app/home/groupes/page.tsx
+++ src/app/home/groupes/page.tsx
@@ -104,12 +104,12 @@ const GroupesPage = () => {
         return (
             <main>
                 <Header username={session.user.username} />
-                <div className="search-container">
+                {/* <div className="search-container">
                     <input type="text" placeholder="Rechercher dans les groupes" />
                     <button>
                         <img src="https://img.icons8.com/material-outlined/24/000000/search.png" alt="Search Icon" />
                     </button>
-                </div>
+                </div> */}
                 <section className="creategrp-section">
                     <h2>Initiateur d&apos;actions collectives ?</h2>
                     <p>Créez votre groupe et rassemblez des citoyens pour agir ensemble pour l&apos;écologie !</p>
